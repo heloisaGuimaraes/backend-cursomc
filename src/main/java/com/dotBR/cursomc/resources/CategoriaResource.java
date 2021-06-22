0727f3f0b6d96d585aefbb55d7cdee54d2bf03ce
Heloisa Guimaraes <heloisaguimaraes86@gmail.com>
2021-06-22 14:55:28 -0300

    Categoria tested

--- src/main/java/com/dotBR/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/dotBR/cursomc/resources/CategoriaResource.java
@@ -1,9 +1,14 @@
 package com.dotBR.cursomc.resources;
 //Classe com as definições REST
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.dotBR.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping (value = "/categorias")//Endpoint que ela irá responder
@@ -11,8 +16,16 @@ public class CategoriaResource {
 	
 	
 	@RequestMapping (method = RequestMethod.GET)//Cada operação deve estar ligada a um verbo do http
-	public String todasAsCategorias() {
-		return "REST funciona!";
+	public List<Categoria> todasAsCategorias() {
+		Categoria c1 = new Categoria (1, "A");
+		Categoria c2 = new Categoria (2, "B");
+		
+		List<Categoria> cats = new ArrayList<>();
+		
+		cats.add(c1);
+		cats.add(c2);
+		
+		return cats;
 	}
 
 }
