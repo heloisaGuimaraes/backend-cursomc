5ce0041abcf1cb7d7aacc0c3145a6c969dc42905
Heloisa Guimaraes <heloisaguimaraes86@gmail.com>
2021-06-22 16:21:56 -0300

    Repository and Service to Categoria created

--- src/main/java/com/dotBR/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/dotBR/cursomc/resources/CategoriaResource.java
@@ -1,9 +1,6 @@
 package com.dotBR.cursomc.resources;
 //Classe com as definições REST
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
