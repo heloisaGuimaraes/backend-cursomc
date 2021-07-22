9fd656eb8f6b390d53fbebd172ee381e8622860f
Heloisa Guimaraes <heloisaguimaraes86@gmail.com>
2021-07-22 16:26:10 -0300

    Inserindo um novo Cliente com POST

--- src/main/java/com/dotBR/cursomc/services/ClienteService.java
+++ src/main/java/com/dotBR/cursomc/services/ClienteService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
@@ -12,9 +14,14 @@ import org.springframework.data.domain.Sort.Direction;
 //classe com a ação do REST
 import org.springframework.stereotype.Service;
 
+import com.dotBR.cursomc.domain.Cidade;
 import com.dotBR.cursomc.domain.Cliente;
+import com.dotBR.cursomc.domain.Endereco;
+import com.dotBR.cursomc.domain.enums.TipoCliente;
 import com.dotBR.cursomc.dto.ClienteDTO;
+import com.dotBR.cursomc.dto.ClienteNewDTO;
 import com.dotBR.cursomc.repositories.ClienteRepository;
+import com.dotBR.cursomc.repositories.EnderecoRepository;
 import com.dotBR.cursomc.services.exceptions.DataIntegrityException;
 
 @Service
@@ -22,6 +29,9 @@ public class ClienteService {
 
 	@Autowired
 	private ClienteRepository repo;
+	
+	@Autowired
+	private EnderecoRepository repoEnd;
 
 	public Cliente findById(Integer id) {
 		Optional<Cliente> cliente = repo.findById(id);
@@ -66,6 +76,30 @@ public class ClienteService {
 	public Cliente fromDTO(ClienteDTO objDto) {
 		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
 
+	}
+
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(),
+				TipoCliente.toEnum(objDto.getTipo()));
+		Cidade cid = new Cidade(objDto.getCidade(), null, null);
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(),
+				objDto.getBairro(), objDto.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+
+		if (objDto.getTelefone2() != null)
+			cli.getTelefones().add(objDto.getTelefone2());
+		if (objDto.getTelefone3() != null)
+			cli.getTelefones().add(objDto.getTelefone3());
+		return cli;
+	}
+
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		repoEnd.saveAll(obj.getEnderecos());
+		return obj;
 	}
 
 }
