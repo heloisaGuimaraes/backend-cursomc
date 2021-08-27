79a3848999a71fb7658c54064edaa85f6ef97de8
Heloisa Guimaraes <heloisaguimaraes86@gmail.com>
2021-08-27 19:00:44 -0300

    Subtotal e total do pedido

--- src/main/java/com/dotBR/cursomc/domain/ItemPedido.java
+++ src/main/java/com/dotBR/cursomc/domain/ItemPedido.java
@@ -32,6 +32,10 @@ public class ItemPedido implements Serializable {
 		this.preco = preco;
 	}
 
+	public double getSubTotal() {
+		return (preco - desconto) * quantidade;
+	}
+
 	public ItemPedidoPK getId() {
 		return id;
 	}
@@ -41,7 +45,6 @@ public class ItemPedido implements Serializable {
 		return this.id.getPedido();
 	}
 
-	
 	public Produto getProduto() {
 		return this.id.getProduto();
 	}
